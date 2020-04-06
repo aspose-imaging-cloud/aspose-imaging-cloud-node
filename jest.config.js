93496776020f4e6c74226adef2ae8b1a9c05fadb
Sergei Zubov <sergei.zubov@aspose.com>
2020-04-06 11:26:40 +0300

    Modify jest config to transform file-type lib

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ["***/test/**/*-tests-*v*.ts"],
   testEnvironment: 'node',
-  setupTestFrameworkScriptFile: './jest.setup.js'
+  setupTestFrameworkScriptFile: './jest.setup.js',
+  transformIgnorePatterns: [
+    "/node_modules/(?!file-type).+\\.js$"
+  ]
 };
