9682336609af4b7992c147d6cf8fe51946f80e13
Dmitry Pasyutin <dmitry.pasyutin@aspose.com>
2020-04-15 13:58:14 +0300

    object detection method and tests added

--- lib/model/model.ts
+++ lib/model/model.ts
@@ -71,6 +71,68 @@ export class BmpProperties {
     }        
 }
 
+export class DetectedObject {
+
+    /**
+     * Attribute type map
+     */
+    public static attributeTypeMap: Array<{
+        /**
+         * Attribute name
+         */
+        name: string, 
+        /**
+         * Attribute base name
+         */
+        baseName: string,
+        /**
+         * Attribute type
+         */
+        type: string}> = [
+        {
+            name: "label",
+            baseName: "Label",
+            type: "string",
+        },        
+        {
+            name: "score",
+            baseName: "Score",
+            type: "number",
+        },        
+        {
+            name: "bounds",
+            baseName: "Bounds",
+            type: "Rectangle",
+        }    ];
+
+    /**
+     * Returns attribute type map
+     */
+    public static getAttributeTypeMap() {
+        return DetectedObject.attributeTypeMap;
+    }
+
+    /**
+     * label
+     */
+    public label: string;
+    
+    /**
+     * score
+     */
+    public score: number;
+    
+    /**
+     * bounds
+     */
+    public bounds: Rectangle;
+    
+    public constructor(init?: Partial<DetectedObject>) {
+        
+        Object.assign(this, init);
+    }        
+}
+
 /**
  * Represents information about image in dicom format.
  */
@@ -3137,6 +3199,78 @@ export class PsdProperties {
     }        
 }
 
+export class Rectangle {
+
+    /**
+     * Attribute type map
+     */
+    public static attributeTypeMap: Array<{
+        /**
+         * Attribute name
+         */
+        name: string, 
+        /**
+         * Attribute base name
+         */
+        baseName: string,
+        /**
+         * Attribute type
+         */
+        type: string}> = [
+        {
+            name: "X",
+            baseName: "X",
+            type: "number",
+        },        
+        {
+            name: "Y",
+            baseName: "Y",
+            type: "number",
+        },        
+        {
+            name: "width",
+            baseName: "Width",
+            type: "number",
+        },        
+        {
+            name: "height",
+            baseName: "Height",
+            type: "number",
+        }    ];
+
+    /**
+     * Returns attribute type map
+     */
+    public static getAttributeTypeMap() {
+        return Rectangle.attributeTypeMap;
+    }
+
+    /**
+     * X
+     */
+    public X: number;
+    
+    /**
+     * Y
+     */
+    public Y: number;
+    
+    /**
+     * width
+     */
+    public width: number;
+    
+    /**
+     * height
+     */
+    public height: number;
+    
+    public constructor(init?: Partial<Rectangle>) {
+        
+        Object.assign(this, init);
+    }        
+}
+
 /**
  * Search context status.
  */
@@ -4994,6 +5128,7 @@ const enumsMap = {
 
 const typeMap = {
             BmpProperties,
+            DetectedObject,
             DicomProperties,
             DiscUsage,
             DjvuProperties,
@@ -5019,6 +5154,7 @@ const typeMap = {
             OdgProperties,
             PngProperties,
             PsdProperties,
+            Rectangle,
             SearchContextStatus,
             SearchResult,
             SearchResultsSet,
@@ -6150,6 +6286,50 @@ export class CreateModifiedWmfRequest {
 }
 
 /**
+ * Request model for CreateObjectBounds operation.
+ */
+export class CreateObjectBoundsRequest {
+    /**
+     * Input image
+     */
+    public imageData: Buffer;
+
+    /**
+     * Object detection method
+     */
+    public method: string;
+
+    /**
+     * Object detection probability threshold in percents
+     */
+    public threshold: number;
+
+    /**
+     * Draw detected objects classes
+     */
+    public includeClass: boolean;
+
+    /**
+     * Draw detected objects scores
+     */
+    public includeScore: boolean;
+
+    /**
+     * Path to updated file (if this is empty, response contains streamed image)
+     */
+    public outPath: string;
+
+    /**
+     * Your Aspose Cloud Storage name.
+     */
+    public storage: string;
+    
+    public constructor(init?: Partial<CreateObjectBoundsRequest>) {        
+        Object.assign(this, init);
+    } 
+}
+
+/**
  * Request model for CreateResizedImage operation.
  */
 export class CreateResizedImageRequest {
@@ -6311,6 +6491,50 @@ export class CreateUpdatedImageRequest {
     public storage: string;
     
     public constructor(init?: Partial<CreateUpdatedImageRequest>) {        
+        Object.assign(this, init);
+    } 
+}
+
+/**
+ * Request model for CreateVisualObjectBounds operation.
+ */
+export class CreateVisualObjectBoundsRequest {
+    /**
+     * Input image
+     */
+    public imageData: Buffer;
+
+    /**
+     * Object detection method
+     */
+    public method: string;
+
+    /**
+     * Object detection probability threshold in percents
+     */
+    public threshold: number;
+
+    /**
+     * Draw detected objects classes
+     */
+    public includeClass: boolean;
+
+    /**
+     * Draw detected objects scores
+     */
+    public includeScore: boolean;
+
+    /**
+     * Path to updated file (if this is empty, response contains streamed image)
+     */
+    public outPath: string;
+
+    /**
+     * Your Aspose Cloud Storage name.
+     */
+    public storage: string;
+    
+    public constructor(init?: Partial<CreateVisualObjectBoundsRequest>) {        
         Object.assign(this, init);
     } 
 }
@@ -7737,6 +7961,50 @@ export class MoveFolderRequest {
 }
 
 /**
+ * Request model for ObjectBounds operation.
+ */
+export class ObjectBoundsRequest {
+    /**
+     * Image file name.
+     */
+    public name: string;
+
+    /**
+     * Object detection method
+     */
+    public method: string;
+
+    /**
+     * Object detection probability threshold in percents
+     */
+    public threshold: number;
+
+    /**
+     * Return detected objects classes
+     */
+    public inlcudeClass: boolean;
+
+    /**
+     * Return detected objects score
+     */
+    public includeScore: boolean;
+
+    /**
+     * Folder
+     */
+    public folder: string;
+
+    /**
+     * Storage
+     */
+    public storage: string;
+    
+    public constructor(init?: Partial<ObjectBoundsRequest>) {        
+        Object.assign(this, init);
+    } 
+}
+
+/**
  * Request model for ObjectExists operation.
  */
 export class ObjectExistsRequest {
@@ -8028,6 +8296,50 @@ export class UploadFileRequest {
     public storageName: string;
     
     public constructor(init?: Partial<UploadFileRequest>) {        
+        Object.assign(this, init);
+    } 
+}
+
+/**
+ * Request model for VisualObjectBounds operation.
+ */
+export class VisualObjectBoundsRequest {
+    /**
+     * The image features detector.
+     */
+    public name: string;
+
+    /**
+     * Object detection method
+     */
+    public method: string;
+
+    /**
+     * Object detection probability threshold in percents
+     */
+    public threshold: number;
+
+    /**
+     * Draw detected objects classes
+     */
+    public includeClass: boolean;
+
+    /**
+     * Draw detected objects scores
+     */
+    public includeScore: boolean;
+
+    /**
+     * The folder.
+     */
+    public folder: string;
+
+    /**
+     * The storage.
+     */
+    public storage: string;
+    
+    public constructor(init?: Partial<VisualObjectBoundsRequest>) {        
         Object.assign(this, init);
     } 
 }
---
+++ test/api/ai/objectdetection-ai-api-tests-v3_0.ts
@@ -0,0 +1,170 @@
+/*
+* --------------------------------------------------------------------------------------------------------------------
+* <copyright company="Aspose">
+*   Copyright (c) 2018-2019 Aspose Pty Ltd. All rights reserved.
+* </copyright>
+* <summary>
+*   Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+* 
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+* 
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+* </summary>
+* --------------------------------------------------------------------------------------------------------------------
+*/
+
+import * as imaging from "../../../lib/api";
+import {ApiTester} from "../../base/api-tester";
+
+/**
+ * Class for testing object detection API calls
+ */
+class ObjectDetectionApiTests extends ApiTester {
+
+    public async objectDetectionImageTest() {
+        const name: string = "object_detection_example.jpg";
+        const folder: string = this.TempFolder;
+        const storage: string = this.TestStorage;
+
+        for (const inputFile of this.InputTestFiles) {
+            if (inputFile.name !== name) {
+                continue;
+            }
+
+            await this.testObjectDetectionGetRequest(
+                    "objectDetectionImageTest",
+                    `Input image: ${name}; `,
+                    name,
+                    async () => {
+                        const request: imaging.ObjectBoundsRequest = new imaging.ObjectBoundsRequest({ name,
+                            folder, storage });
+                        const result = await this.imagingApi.objectBounds(request);
+                        return result;
+                    },
+                    (detectedObjects) => {
+                        expect(detectedObjects).toBeTruthy();
+                        expect(detectedObjects.length).toBeGreaterThan(0);
+                        return Promise.resolve();
+                    },
+                    folder,
+                    storage);
+
+            await this.testGetRequest(
+                "visualObjectDetectionImageTest",
+                `Input image: ${name}; `,
+                name,
+                async () => {
+                    const request: imaging.VisualObjectBoundsRequest = new imaging.VisualObjectBoundsRequest({ name,
+                        folder, storage });
+                    return await this.imagingApi.visualObjectBounds(request);
+                },
+                (imagingResponse) => {
+                    expect(imagingResponse).toBeTruthy();
+                    return Promise.resolve();
+                },
+                folder,
+                storage);
+
+        }
+    }
+
+    public async createObjectDetectionTest(
+        saveResultToStorage: boolean,
+        ) {
+        const name: string = "object_detection_example.jpg";
+        const folder: string = this.TempFolder;
+        const storage: string = this.TestStorage;
+
+        for (const inputFile of this.InputTestFiles) {
+            if (inputFile.name !== name) {
+                continue;
+            }
+
+            await this.testObjectDetectionPostRequest(
+                "createObjectBoundsTest",
+                saveResultToStorage,
+                `Input image: ${name};`,
+                name,
+                "object_detection_result.jpg",
+                async (inputStream, outPath) => {
+                    const request: imaging.CreateObjectBoundsRequest
+                        = new imaging.CreateObjectBoundsRequest(
+                        { imageData: inputStream, includeClass: true, includeScore: true, outPath, storage });
+                    return await this.imagingApi.createObjectBounds(request);
+                },
+                (detectedObjects) => {
+                    expect(detectedObjects).toBeTruthy();
+                    expect(detectedObjects.length).toBeGreaterThan(0);
+                    return Promise.resolve();
+                },
+                folder,
+                storage);
+
+
+            await this.testPostRequest(
+                    "createVisualObjectBoundsTest",
+                    saveResultToStorage,
+                    `Input image: ${name};`,
+                    name,
+                    "object_detection_result.jpg",
+                    async (inputStream, outPath) => {
+                        const request: imaging.CreateVisualObjectBoundsRequest
+                            = new imaging.CreateVisualObjectBoundsRequest(
+                                { imageData: inputStream, includeClass: true, includeScore: true, outPath, storage });
+                        return await this.imagingApi.createVisualObjectBounds(request);
+                    },
+                    (originalProperties, resultProperties) => {
+                        expect(originalProperties).toBeTruthy();
+                        expect(resultProperties).toBeTruthy();
+                        return Promise.resolve();
+                    },
+                    folder,
+                    storage);
+        }
+    }
+}
+
+const testClass: ObjectDetectionApiTests = new ObjectDetectionApiTests();
+
+beforeEach(() => {
+    jest.setTimeout(ApiTester.DefaultTimeout);
+});
+
+beforeAll(async () =>  {
+    await testClass.beforeAll();
+});
+
+afterAll(async () =>  {
+    await testClass.afterAll();
+});
+
+describe.each([true, false])(
+    "ObjectDetectionTestSuite_V3",
+    (saveResultToStorage) => {
+        if (!saveResultToStorage) {
+            test(`objectDetectionTest`, async () => {
+                await testClass.objectDetectionImageTest();
+            });
+        }
+
+        test(`createobjectDetectionTest: saveResultToStorage - ${saveResultToStorage}`, async () => {
+            await testClass.createObjectDetectionTest(saveResultToStorage);
+        });
+
+        beforeEach(() => {
+            jest.setTimeout(ApiTester.DefaultTimeout);
+        });
+    },
+);
