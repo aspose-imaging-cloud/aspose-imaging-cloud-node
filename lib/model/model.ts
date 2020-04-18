169fe0d82a38627f1160c14d948ac8f7b1dfced4
aspose-imaging-cloud-deployer <alexander.feigin@aspose.com>
2020-04-18 13:02:07 -0500

    SDK regenerated by CI server

--- lib/model/model.ts
+++ lib/model/model.ts
@@ -5469,6 +5469,80 @@ export class CreateImageFrameRequest {
 }
 
 /**
+ * Request model for CreateImageFrameRange operation.
+ */
+export class CreateImageFrameRangeRequest {
+    /**
+     * Input image
+     */
+    public imageData: Buffer;
+
+    /**
+     * Index of the first frame in range.
+     */
+    public startFrameId: number;
+
+    /**
+     * Index of the last frame in range.
+     */
+    public endFrameId: number;
+
+    /**
+     * New width.
+     */
+    public newWidth: number;
+
+    /**
+     * New height.
+     */
+    public newHeight: number;
+
+    /**
+     * X position of start point for cropping rectangle.
+     */
+    public x: number;
+
+    /**
+     * Y position of start point for cropping rectangle.
+     */
+    public y: number;
+
+    /**
+     * Width of cropping rectangle.
+     */
+    public rectWidth: number;
+
+    /**
+     * Height of cropping rectangle.
+     */
+    public rectHeight: number;
+
+    /**
+     * RotateFlip method (Rotate180FlipNone, Rotate180FlipX, Rotate180FlipXY, Rotate180FlipY, Rotate270FlipNone, Rotate270FlipX, Rotate270FlipXY, Rotate270FlipY, Rotate90FlipNone, Rotate90FlipX, Rotate90FlipXY, Rotate90FlipY, RotateNoneFlipNone, RotateNoneFlipX, RotateNoneFlipXY, RotateNoneFlipY). Default is RotateNoneFlipNone.
+     */
+    public rotateFlipMethod: string;
+
+    /**
+     * If result will include all other frames or just a specified frame.
+     */
+    public saveOtherFrames: boolean;
+
+    /**
+     * Path to updated file (if this is empty, response contains streamed image).
+     */
+    public outPath: string;
+
+    /**
+     * Your Aspose Cloud Storage name.
+     */
+    public storage: string;
+    
+    public constructor(init?: Partial<CreateImageFrameRangeRequest>) {        
+        Object.assign(this, init);
+    } 
+}
+
+/**
  * Request model for CreateImageSearch operation.
  */
 export class CreateImageSearchRequest {
@@ -6876,6 +6950,80 @@ export class GetImageFramePropertiesRequest {
     public storage: string;
     
     public constructor(init?: Partial<GetImageFramePropertiesRequest>) {        
+        Object.assign(this, init);
+    } 
+}
+
+/**
+ * Request model for GetImageFrameRange operation.
+ */
+export class GetImageFrameRangeRequest {
+    /**
+     * Filename of image.
+     */
+    public name: string;
+
+    /**
+     * Index of the first frame in range.
+     */
+    public startFrameId: number;
+
+    /**
+     * Index of the last frame in range.
+     */
+    public endFrameId: number;
+
+    /**
+     * New width.
+     */
+    public newWidth: number;
+
+    /**
+     * New height.
+     */
+    public newHeight: number;
+
+    /**
+     * X position of start point for cropping rectangle.
+     */
+    public x: number;
+
+    /**
+     * Y position of start point for cropping rectangle.
+     */
+    public y: number;
+
+    /**
+     * Width of cropping rectangle.
+     */
+    public rectWidth: number;
+
+    /**
+     * Height of cropping rectangle.
+     */
+    public rectHeight: number;
+
+    /**
+     * RotateFlip method (Rotate180FlipNone, Rotate180FlipX, Rotate180FlipXY, Rotate180FlipY, Rotate270FlipNone, Rotate270FlipX, Rotate270FlipXY, Rotate270FlipY, Rotate90FlipNone, Rotate90FlipX, Rotate90FlipXY, Rotate90FlipY, RotateNoneFlipNone, RotateNoneFlipX, RotateNoneFlipXY, RotateNoneFlipY). Default is RotateNoneFlipNone.
+     */
+    public rotateFlipMethod: string;
+
+    /**
+     * If result will include all other frames or just a specified frame.
+     */
+    public saveOtherFrames: boolean;
+
+    /**
+     * Folder with image to process.
+     */
+    public folder: string;
+
+    /**
+     * Your Aspose Cloud Storage name.
+     */
+    public storage: string;
+    
+    public constructor(init?: Partial<GetImageFrameRangeRequest>) {        
         Object.assign(this, init);
     } 
 }
