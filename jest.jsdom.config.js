1e355f7ae24539f381d8c7fa4a0da63b117b64fd
Sergei Zubov <sergei.zubov@aspose.com>
2020-10-07 10:37:34 +0300

    Fix overlapping test report names

--- jest.jsdom.config.js
+++ jest.jsdom.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
-  testMatch: ["***/test/**/*-tests-*v*.ts"],
-  testEnvironment: 'jsdom',
-  setupTestFrameworkScriptFile: './jest.setup.js'
+    preset: 'ts-jest',
+    testMatch: ["***/test/**/*-tests-*v*.ts"],
+    testEnvironment: 'jsdom',
+    setupTestFrameworkScriptFile: './jest.setup.js',
+    reporters: [
+        'default',
+        ['jest-junit', {
+            outputDirectory: 'test_reports',
+            outputName: 'jest-junit-jsdom.xml',
+        }]
+    ]
 };
--- jest.node.config.js
+++ jest.node.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
-  testMatch: ["***/test/**/*-tests-*v*.ts"],
-  testEnvironment: 'node',
-  setupTestFrameworkScriptFile: './jest.setup.js'
+    preset: 'ts-jest',
+    testMatch: ["***/test/**/*-tests-*v*.ts"],
+    testEnvironment: 'node',
+    setupTestFrameworkScriptFile: './jest.setup.js',
+    reporters: [
+        'default',
+        ['jest-junit', {
+            outputDirectory: 'test_reports',
+            outputName: 'jest-junit-node.xml',
+        }]
+    ]
 };
