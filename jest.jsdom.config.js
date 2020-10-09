a6edec71f975847ab5c12081414013bcc66eb341
Sergei Zubov <sergei.zubov@aspose.com>
2020-10-09 15:21:27 +0300

    Remove duplicates of test report config

--- jest.jsdom.config.js
+++ jest.jsdom.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
-    testMatch: ["***/test/**/*-tests-*v*.ts"],
     testEnvironment: 'jsdom',
     setupTestFrameworkScriptFile: './jest.setup.js',
     reporters: [
--- jest.node.config.js
+++ jest.node.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
-    testMatch: ["***/test/**/*-tests-*v*.ts"],
     testEnvironment: 'node',
     setupTestFrameworkScriptFile: './jest.setup.js',
     reporters: [
